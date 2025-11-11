f9fe3f7de8c0b6de14d0e1b3b832a958e33d0a5d
Taegeuni <qoxorms1504@gmail.com>
2025-11-11 14:58:50 +0900

    style(back): 에러메세지에 마침표(.,)등으로 생기는 경고문구 제거
    에러 메시지 문자열은 마침표(.), 느낌표(!), 줄바꿈(\n) 등으로 끝나면 안 된다는 경고문구 때문에 메세지 수정

--- internal/app/services/profile_service.go
+++ internal/app/services/profile_service.go
@@ -13,7 +13,7 @@ func GetMyProfile(userID uint) (map[string]interface{}, error) {
 func UpdateProfile(userID uint, nickname string, profileImage *string) (models.User, error) {
 	user, err := repositories.FindUserByID(userID)
 	if err != nil {
-		return user, errors.New("유저를 찾을 수 없습니다.")
+		return user, errors.New("유저를 찾을 수 없습니다")
 	}
 
 	updates := map[string]interface{}{}
@@ -27,12 +27,12 @@ func UpdateProfile(userID uint, nickname string, profileImage *string) (models.U
 	}
 
 	if len(updates) == 0 {
-		return user, errors.New("변경할 정보가 없습니다.")
+		return user, errors.New("변경할 정보가 없습니다")
 	}
 
 	err = repositories.UpdateUserFields(&user, updates)
 	if err != nil {
-		return user, errors.New("프로필 업데이트에 실패했습니다.")
+		return user, errors.New("프로필 업데이트에 실패했습니다")
 	}
 
 	// 메모리 반영
